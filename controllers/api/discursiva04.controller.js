480c0ea4924774fa74b0339d4e092115b9f92b4b
Johnny Messias Paulino <90730926+JohnnyMessiasPaulino@users.noreply.github.com>
2023-04-25 07:37:40 -0300

    Algoritmos feitos em sala de aula adaptados para NodeJS

--- controllers/api/discursiva04.controller.js
+++ controllers/api/discursiva04.controller.js
@@ -11,9 +11,12 @@ function iterativa(req, res) {
    var m = parseInt(req.query.m);
    var n = parseInt(req.query.n);
    let resultado = 1;
+   let i = m;
    if ((m <= n)&& (m>0)) {
-      for (i = m; i<=n; i++){
+
+      while (i<=n){
          resultado *= (i + (1/i));
+         i++;
       }
       var retorno = resultado;
       res.send(
@@ -28,10 +31,16 @@ function iterativa(req, res) {
 }
 
 function produtorioRecursivo(m, n) {
-   if (m === n)
-      return (n + (1/n));
-   else
+   let i = m;
+   let ultimaRecursao = false;
+
+   if(m<n)
       return (m + (1/m)) * produtorioRecursivo(m+1, n);
+   else
+      ultimaRecursao = true;
+
+   if (ultimaRecursao)
+      return (n + (1/n));
 }
 
 function recursiva(req, res) {
